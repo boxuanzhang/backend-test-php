e0a51f313458c40e51a190011446736674546ae8
Boxuan Zhang <boxuan.zhang@student.qut.edu.au>
2018-03-06 22:59:38 +1000

    add auth and add middleware to api

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,21 @@
 |
 */
 
-// TODO: Need to protect routes with an API key which identifies the User
 // accessing the route... except maybe the route for registering ...
-Route::group(['namespace' => 'Api', 'prefix' => 'v1'], function () {
-    // Sections
-    Route::apiResource('sections', 'SectionController');
+Route::group(['namespace' => 'Api', 'prefix' => 'v1', 'middleware' => ['auth:api']], function() {
+	// Sections
+	Route::apiResource('sections', 'SectionController');
 
-    // Messages
-    Route::apiResource('messages', 'MessageController');
+	// Messages
+	Route::apiResource('messages', 'MessageController');
 
-    // Topics
-    Route::get('/topics/{topic}/thread', 'TopicThreadController@show');
-    Route::apiResource('topics', 'TopicController');
+	// Topics
+	Route::get('/topics/{topic}/thread', 'TopicThreadController@show');
+	Route::apiResource('topics', 'TopicController');
 
 
-    // Users
-    Route::get('/users/{user}/profile', 'UserProfileController@show');
-    Route::patch('/users/{user}/profile', 'UserProfileController@update');
-    Route::apiResource('users', 'UserController');
+	// Users
+	Route::get('/users/{user}/profile', 'UserProfileController@show');
+	Route::patch('/users/{user}/profile', 'UserProfileController@update');
+	Route::apiResource('users', 'UserController');
 });
