7533f2f2946e6a6918f69090063159cbc917d3b3
Boxuan Zhang <boxuan.zhang@student.qut.edu.au>
2018-03-06 22:52:21 +1000

    change seeder of message

--- database/seeds/MessageSeeder.php
+++ database/seeds/MessageSeeder.php
@@ -2,15 +2,39 @@
 
 use Illuminate\Database\Seeder;
 
+/**
+ * Class MessageSeeder
+ */
 class MessageSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        factory(\App\Models\Message::class, 20)->create();
-    }
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		\App\Models\Message::truncate();
+
+		$topic = factory(\App\Models\Topic::class)->create();
+
+		for ($i = 0; $i < 20; $i++) {
+			$this->helper(0, $topic->id);
+		}
+	}
+
+	public function helper($parent_id = 0, $topic_id)
+	{
+		$hasChildren = random_int(0, 2);
+
+		$message = factory(\App\Models\Message::class)->create([
+			'topic_id'  => $topic_id,
+			'parent_id' => $parent_id,
+		]);
+
+		if ($hasChildren) {
+			$this->helper($message->id, $topic_id);
+		}
+	}
 }
